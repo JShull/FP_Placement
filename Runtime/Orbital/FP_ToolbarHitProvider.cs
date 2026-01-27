088006da543bdc3fa80dcd46a2dd9de4f5a59277
JShull <the.john.shull@gmail.com>
2026-01-27 11:22:01 -0500

    Update FP_ToolbarHitProvider.cs
    * Added On Start Delay Event

--- Runtime/Orbital/FP_ToolbarHitProvider.cs
+++ Runtime/Orbital/FP_ToolbarHitProvider.cs
@@ -7,8 +7,19 @@ namespace FuzzPhyte.Placement.OrbitalCamera
     public sealed class FP_ToolbarHitProvider : MonoBehaviour
     {
         [SerializeField] private FP_ToolbarAction _action;
+        [Tooltip("Event invoked on selection")]
         public UnityEvent OnSelectionEvent;
-
+        [Tooltip("Delay an action after start some 'x' seconds")]
+        [SerializeField] private bool DelayActionOnStart = false;
+        [SerializeField] private float DelaySeconds = 1f;
+        public UnityEvent OnStartDelayedEvent;
+        public void Start()
+        {
+            if (DelayActionOnStart)
+            {
+                Invoke(nameof(OnStartDelayedEvent), DelaySeconds);
+            }
+        }
         public FP_ToolbarAction Action => _action;
 
         // Optional: hover/selection visual hooks like your cube provider
